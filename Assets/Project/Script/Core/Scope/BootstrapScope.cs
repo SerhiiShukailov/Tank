51c24ff656935c66c0857a3dd5544d93514d03fb
Serhii.Shukailov <l2.kidan@gmail.com>
2026-06-12 16:24:14 +0300

    delete temp scene and script

--- Assets/Project/Script/Core/Scope/BootstrapScope.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using VContainer;
-using VContainer.Unity;
-public sealed class BootstrapScope : LifetimeScope {
-  protected override void Configure (IContainerBuilder builder) {
-    Debug.LogWarning("BootstrapScope Configure");
-  }
-}
